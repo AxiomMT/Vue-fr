3e863fb2f10da7ba03fa60c271a1d6e5c9ac0d00
AxiomMT <pierrelemen@hotmail.com>
2022-08-31 20:19:18 +0200

    updated MenuItem.vue

--- cafe-with-a-vue/cli-version/src/components/MenuItem.vue
+++ cafe-with-a-vue/cli-version/src/components/MenuItem.vue
@@ -1,20 +1,21 @@
 <script>
 export default {
-    name: 'MenuItem'
+    name: 'MenuItem',
+    props: ['name', 'inStock', 'quantity', 'imageSrc', 'imageAlt']
 }
 </script>
 
 <template>
     <div>    
-        <img class="menu-item__image" :src="item.image.source" :alt="item.image.alt" />
+        <img class="menu-item__image" :src="imageSrc" :alt="imageAlt" />
         <div>
-            <h3>{{ item.name }}</h3>
-            <p v-if="item.inStock">En stock</p>
+            <h3>{{ name }}</h3>
+            <p v-if="inStock">En stock</p>
             <p v-else>En rupture de stock</p>
             <div>
-                <label for="add-item-quantity">Quantité : {{ item.quantity }}</label>
-                <input v-model.number="item.quantity" id="add-item-quantity" type="number" />
-                <button @click="addToShoppingCart(item.quantity)">Ajouter au panier d'achat</button>
+                <label for="add-item-quantity">Quantité : {{ quantity }}</label>
+                <input v-model.number="quantity" id="add-item-quantity" type="number" />
+                <button @click="addToShoppingCart(quantity)">Ajouter au panier d'achat</button>
             </div>
         </div>
     </div>
