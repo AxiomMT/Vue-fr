8dd6d5085cf22f5e8fe64e226615e6806f536ba4
AxiomMT <pierrelemen@hotmail.com>
2022-08-31 21:42:33 +0200

    Copied content from Backup project folder

--- cafe-with-a-vue/cli-version/src/main.js
+++ cafe-with-a-vue/cli-version/src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+createApp(App).mount('#app')
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+// import Vue from 'vue'
+// import App from './App.vue'
+
+// Vue.config.productionTip = false
+
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
