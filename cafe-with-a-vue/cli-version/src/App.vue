f0171dd06601da7fccf53e9c3f41350d9ccc3ad2
AxiomMT <pierrelemen@hotmail.com>
2022-09-01 14:36:43 +0200

    add vue router

--- cafe-with-a-vue/cli-version/src/App.vue
+++ cafe-with-a-vue/cli-version/src/App.vue
@@ -1,132 +1,30 @@
 <template>
-  <div id="app" class="app">
-    <h1>{{ restaurantName }}</h1>
-    <p class="description">
-      Bienvenue dans notre café {{ restaurantName }}! Nous sommes réputés pour
-      notre pain et nos merveilleuses pâtisseries. Faites vous plaisir dès le
-      matin ou avec un goûter réconfortant. Mais attention, vous verrez qu'il
-      est difficile de s'arrêter.
-    </p>
-
-    <section class="menu">
-      <h2>Menu</h2>
-      <div v-for="item in simpleMenu" :key="item.name" class="menu-item">
-        <MenuItem 
-          :name="item.name" 
-          :inStock="item.inStock" 
-          :quantity="item.quantity" 
-          :imageSrc="item.image.src"
-          :imageAlt="item.image.alt" 
-          @addItem="addToShoppingCart(item.quantity)"
-        />
-      </div>
-    </section>
-
-    <aside class="shopping-cart">
-      <h2>Panier d'achat : {{ shoppingCart }} articles</h2>
-    </aside>
-
-    <h2>Contactez nous</h2>
-    <p>Adresse : {{ address }}</p>
-    <p>Téléphone : {{ phone }}</p>
-    <p>Email : {{ email }}</p>
-    <p>Horaires :</p>
-    <ul>
-      <li>L-V: 06:00 à 16:00</li>
-      <li>Samedi: 07:00 à 14:00</li>
-      <li>Dimanche: 07:00 à 12:00</li>
-    </ul>
-    <footer class="footer">
-      <p>{{ copyright }}</p>
-    </footer>
-  </div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script>
-import MenuItem from './components/MenuItem.vue'
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  name: 'App',
-  components: {
-    MenuItem
-  },
-  data() {
-    return {
-      address: "18 avenue du Beurre, Paris, France",
-      email: "hello@cafewithavue.bakery",
-      phone: "01 88 88 88 88",
-      restaurantName: "La belle vue",
-      shoppingCart: 0,
-      simpleMenu: [
-        {
-          name: "Croissant",
-          image: {
-            src: "/images/croissant.jpg",
-            alt: "Un croissant"
-          },
-          inStock: true,
-          quantity: 1
-        },
-        {
-          name: "Baguette de pain",
-          image: {
-            src: "/images/french-baguette.jpeg",
-            alt: "Quatre baguettes de pain"
-          },
-          inStock: true,
-          quantity: 1
-        },
-        {
-          name: "Éclair",
-          image: {
-            src: "/images/eclair.jpg",
-            alt: "Éclair au chocolat"
-          },
-          inStock: false,
-          quantity: 1
-        }
-      ]
-    }
-  },
-  computed: {
-    copyright() {
-      const currentYear = new Date().getFullYear()
+nav {
+  padding: 30px;
 
-      return `Copyright ${this.restaurantName} ${currentYear}`
-    }
-  },
-  methods: {
-    addToShoppingCart(amount) {
-      this.shoppingCart += amount
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
     }
   }
 }
-</script>
-
-<style>
-  .app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
-
-  .description {
-    max-width: 960px;
-    font-size: 1.2rem;
-    margin: 0 auto;
-  }
-
-  .footer {
-    text-align: center;
-    font-style: italic;
-  }
-
-  .menu {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
 </style>
--- cafe-with-a-vue/cli-version/src/main.js
+++ cafe-with-a-vue/cli-version/src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
 
 // import Vue from 'vue'
 // import App from './App.vue'
