224ff8d8ad3a5c21cf44c3b70c1b07607cad8d53
AxiomMT <pierrelemen@hotmail.com>
2022-09-16 12:05:53 +0200

    main.js and router.index.js to Vue 3 update

--- cafe-with-a-vue/cli-version/src/router/index.js
+++ cafe-with-a-vue/cli-version/src/router/index.js
@@ -1,34 +1,27 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Contact from '../views/Contact.vue'
-
-Vue.use(VueRouter)
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import ContactView from '../views/ContactView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: HomeView
   },
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
+    component: () => import('../views/AboutView.vue')
+  }, 
   {
     path: '/contact',
-    name: 'contact',
-    component: Contact
+    name: 'contact', 
+    component: ContactView
   }
 ]
 
-const router = new VueRouter({
-  mode: 'history',
+const router = createRouter({
+  history: createWebHistory(),
   routes
 })
 
